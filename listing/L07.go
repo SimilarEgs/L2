7878c59dccbad5756f764f3dfcf39e73001c5db2
SimilarEgs <newfrontierorder@gmail.com>
2022-09-16 11:58:30 +0300

    finished 7/7 listing

--- listing/L07.go
+++ listing/L07.go
@@ -1,4 +1,15 @@
 // Что выведет программа? Объяснить вывод программы.
+//
+// Ответ:
+// Последовательность чисел с новой строки от 1 до 8, а затем «бесконечное» кол-во нулей
+//
+// Почему:
+// В цикле for-range мы читаем канал с, который в то же время, получает некие значения из каналов (a, b)
+// Далее, в канал c перестают поступать данны и мы «бесконечно» выводим значение по умолчанию канала c, то бишь 0
+//
+// Решение:
+// В функции merge задать циклу булевое условие для его завершения.
+// Когда каналы a, b закроются мы выйдем из цикла, и закроем канал c.
 
 package main
 
@@ -8,41 +19,102 @@ import (
 	"time"
 )
 
+func main() {
+	a := asChan(1, 3, 5, 7)
+	b := asChan(2, 4, 6, 8)
+	c := merge(a, b)
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+}
+
 func asChan(vs ...int) <-chan int {
 	c := make(chan int)
 
 	go func() {
 		for _, v := range vs {
 			c <- v
-			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+			time.Sleep(time.Duration(rand.Intn(1000)))
 		}
-
 		close(c)
 	}()
+
 	return c
 }
 
 func merge(a, b <-chan int) <-chan int {
 	c := make(chan int)
+
+	aDone, bDone := false, false
+
 	go func() {
-		for {
+		defer close(c)
+
+		for !aDone || !bDone {
+
 			select {
-			case v := <-a:
+			case v, ok := <-a:
+				if !ok {
+					aDone = true
+					continue
+				}
 				c <- v
-			case v := <-b:
+			case v, ok := <-b:
+				if !ok {
+					bDone = true
+					continue
+				}
 				c <- v
 			}
 		}
 	}()
+
 	return c
 }
 
-func main() {
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
 
-	a := asChan(1, 3, 5, 7)
-	b := asChan(2, 4, 6, 8)
-	c := merge(a, b)
-	for v := range c {
-		fmt.Println(v)
-	}
-}
+// func asChan(vs ...int) <-chan int {
+// 	c := make(chan int)
+
+// 	go func() {
+// 		for _, v := range vs {
+// 			c <- v
+// 			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+// 		}
+
+// 		close(c)
+// 	}()
+// 	return c
+// }
+
+// func merge(a, b <-chan int) <-chan int {
+// 	c := make(chan int)
+// 	go func() {
+// 		for {
+// 			select {
+// 			case v := <-a:
+// 				c <- v
+// 			case v := <-b:
+// 				c <- v
+// 			}
+// 		}
+// 	}()
+// 	return c
+// }
+
+// func main() {
+
+// 	a := asChan(1, 3, 5, 7)
+// 	b := asChan(2, 4, 6, 8)
+// 	c := merge(a, b)
+// 	for v := range c {
+// 		fmt.Println(v)
+// 	}
+// }
