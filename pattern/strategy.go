94c99b04f8c09912cafc606b376c8b782e3fc5bc
Lex <newfrontierorder@gmail.com>
2022-10-10 14:54:14 +0300

    Update strategy.go

--- pattern/strategy.go
+++ pattern/strategy.go
@@ -41,13 +41,13 @@ import "fmt"
 //  Конкретая стратегии - сложение, вычитание, деление и т.д
 //  Контекст - калькулятор
 //
-                                                           
-				      «interface»	     | strategy 1  |
-  |  Context   |     strategy       |  Strategy   |          | algorithm() |
-  |------------|  ----------------> |-----------  |  <-------|/////////////|
-  | operation()|                    | algorithm() |	     |
-                                                             | strategy 2  | 
-							     | algorithm() |
+//                                                           
+//   				       «interface»	      | strategy 1  |
+// |  Context   |     strategy       |  Strategy   |          | algorithm() |
+// |------------|  ----------------> |-----------  | <--------|/////////////|
+// | operation()|                    | algorithm() |	      |
+//                                                            | strategy 2  | 
+//							      | algorithm() |
 
 type Authorization interface {
 	Auth() error
